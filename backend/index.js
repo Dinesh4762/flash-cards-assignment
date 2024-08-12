b749bc8b4ac973b357ac0a95e7126edeb4cfe248
Dinesh Kumar <kumar62georgian@gmail.com>
2024-08-13 03:11:38 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import { getCards, editCard, deleteCard, createCard, getCard } from "./db.js";
 const app = express();
+app.use(cors());
 const PORT = 8080;
 app.use(express.json());
-app.use(cors());
 
 
 app.get("/",(req,res) =>{
